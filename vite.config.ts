365dce4b49ef2a8bacb46e99bf4a41f591d80939
Ivanna Liuba <iva147iva147@gmail.com>
2023-08-30 15:58:54 +0200

    fix error relate to defaults import with signals via deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,9 @@ import { fileURLToPath, URL } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      jsxRuntime: 'classic',
+    }),
     svgr(),
     checker({
       typescript: true,
